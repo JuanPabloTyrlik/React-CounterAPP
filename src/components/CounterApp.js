f7e90d5d78640fbc459bf09b81349761216a09fe
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2020-07-26 15:41:35 -0300

    CounterApp

---
+++ src/components/CounterApp.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const CounterApp = ({ value = 0 }) => {
+    const [counter, setCounter] = useState(value);
+
+    const handleAdd = () => {
+        setCounter(counter + 1);
+    };
+
+    const handleSubstract = () => {
+        setCounter(counter - 1);
+    };
+
+    const handleReset = () => {
+        setCounter(value);
+    };
+
+    return (
+        <>
+            <h1>CounterApp</h1>
+            <h2>{counter}</h2>
+            <button onClick={handleAdd} style={{width: '55px'}}>+1</button>
+            <button onClick={handleSubstract} style={{width: '55px'}}>-1</button>
+            <button onClick={handleReset}>Reset</button>
+        </>
+    );
+};
+
+CounterApp.propTypes = {
+    value: PropTypes.number.isRequired,
+};
+
+export default CounterApp;
